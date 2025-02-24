1784e80c3318c8e8cbdfdad6ca70f9596d58260d
Victor <victor.acuna.cortez@gmail.com>
2025-02-24 10:24:07 -0300

    Code

---
+++ src/main/java/com/citibank/transactionsclient/TransactionsClientApplication.java
@@ -0,0 +1,13 @@
+package com.citibank.transactionsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionsClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/citibank/transactionsclient/TransactionsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citibank.transactionsclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
